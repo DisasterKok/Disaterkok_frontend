8565515c153f763e7e7c2480247bb176bdb8e6ad
Woorim Kim <wrkimlimi@gmail.com>
2023-10-30 14:04:39 +0900

    feat: apply safe-area-context for status bar area

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,13 @@
 import React, { useEffect, useRef } from 'react';
-import { Text, View, StyleSheet, ScrollView, Animated } from 'react-native';
+import { Text, View, StyleSheet, ScrollView, Animated, Platform } from 'react-native';
 import COLOR from '../constants/colors';
+import { SafeAreaProvider, SafeAreaView, useSafeAreaInsets } from 'react-native-safe-area-context';
 
 export default function Home() {
+  const insets = useSafeAreaInsets();
   const scrollY = useRef(new Animated.Value(0)).current;
 
-  // Define the scroll threshold where you want to change the header color
-  const colorChangeThreshold = 200; // Adjust this value as needed
+  const colorChangeThreshold = 120;
 
   // Function to interpolate the background color
   const headerBackgroundColor = scrollY.interpolate({
@@ -22,36 +23,39 @@ export default function Home() {
   });
 
   return (
-    <View style={styles.layout}>
-      <Animated.View
-        style={{
-          backgroundColor: headerBackgroundColor,
-          position: 'absolute',
-          justifyContent: 'center',
-          alignItems: 'center',
-          top: 0,
-          left: 0,
-          right: 0,
-          height: 60,
-        }}
-      >
-        <Animated.Text style={{ color: headerTextColor }}>앱바 영역입니다</Animated.Text>
-      </Animated.View>
-      <ScrollView
-        onScroll={Animated.event([{ nativeEvent: { contentOffset: { y: scrollY } } }], {
-          useNativeDriver: false,
-        })}
-        scrollEventThrottle={16} // Adjust as needed
-        style={{ position: 'absolute', top: 60, width: '100%' }}
-      >
-        <View style={{ height: 120, backgroundColor: `${COLOR.primary}` }}>
-          <Text>날씨 영역</Text>
-        </View>
-        <View style={styles.contentSheet}>
-          <Text>내용입니다</Text>
-        </View>
-      </ScrollView>
-    </View>
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.layout}>
+        <Animated.View
+          style={[
+            {
+              backgroundColor: headerBackgroundColor,
+            },
+            styles.header,
+          ]}
+        >
+          {/* 상단바 영역 */}
+          <View style={{ width: '100%', height: insets.top }}></View>
+          {/* 앱바 영역 */}
+          <View style={{ height: 42, justifyContent: 'center' }}>
+            <Animated.Text style={{ color: headerTextColor }}>앱바 영역 입니다</Animated.Text>
+          </View>
+        </Animated.View>
+        <ScrollView
+          onScroll={Animated.event([{ nativeEvent: { contentOffset: { y: scrollY } } }], {
+            useNativeDriver: false,
+          })}
+          scrollEventThrottle={16}
+          style={{ marginTop: 42, width: '100%' }}
+        >
+          <View style={{ height: 146, backgroundColor: `${COLOR.primary}` }}>
+            <Text>날씨영역</Text>
+          </View>
+          <View style={styles.contentSheet}>
+            <Text>내용입니다</Text>
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
 
@@ -64,13 +68,34 @@ const styles = StyleSheet.create({
     backgroundColor: `${COLOR.primary}`,
     position: 'relative',
   },
+  header: {
+    position: 'absolute',
+    justifyContent: 'center',
+    alignItems: 'center',
+    top: 0,
+    left: 0,
+    right: 0,
+    display: 'flex',
+    flexDirection: 'column',
+  },
   contentSheet: {
+    display: 'flex',
     width: '100%',
-    height: 1200,
+    height: 3000,
     backgroundColor: `${COLOR.lightGray}`,
-    borderTopColor: `${COLOR.middleGray}`,
     borderTopLeftRadius: 20,
     borderTopRightRadius: 20,
     paddingTop: 20,
+    ...Platform.select({
+      ios: {
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.1,
+        shadowRadius: 10,
+      },
+      android: {
+        elevation: 2,
+      },
+    }),
   },
 });
