ea0c5a0f3aa8c1a8e3ab1cec62868800f8b59627
cychann <skcbdlhc129@naver.com>
2023-11-13 14:55:55 +0900

    Design: 알림 페이지 UI 제작

--- src/pages/Setting.tsx
+++ src/pages/Setting.tsx
@@ -1,10 +1,301 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, Pressable, ScrollView, Platform } from 'react-native';
+import FeatherIcon from 'react-native-vector-icons/Feather';
+import FaIcon from 'react-native-vector-icons/FontAwesome';
+
+import COLOR from '../constants/colors';
+
+const NOTI_DATA = [
+  {
+    id: 1,
+    date: '2023-11-12 09:30:12',
+    catecory: '전국',
+    title: '재난재난제목',
+    content:
+      '재난에 대한 상세 내용 재난에 대한 상세 내용재난에 대한 상세 내용 재난에 대한 상세 내용재난에 대한 상세 내용 재난에 대한 상세 내용재난에 대한 상세 내용 재난에 대한 상세 내용재난에 대한 상세 내용',
+  },
+  {
+    id: 2,
+    date: '2023-11-13 09:30 09:30:12',
+    catecory: '우리동네',
+    title: '재난재난제목',
+    content:
+      '재난에 대한 상세 내용 재난에 대한 상세 내용재난에 대한 상세 내용 재난에 대한 상세 내용재난에 대한 상세 내용 재난에 대한 상세 내용재난에 대한 상세 내용 재난에 대한 상세 내용재난에 대한 상세 내용',
+  },
+  {
+    id: 3,
+    date: '2023-11-13 09:30 09:30:12',
+    catecory: '전국',
+    title: '재난재난제목',
+    content:
+      '재난에 대한 상세 내용 재난에 대한 상세 내용재난에 대한 상세 내용 재난에 대한 상세 내용재난에 대한 상세 내용 재난에 대한 상세 내용재난에 대한 상세 내용 재난에 대한 상세 내용재난에 대한 상세 내용',
+  },
+  {
+    id: 4,
+    date: '2023-11-13 09:30 09:30:12',
+    catecory: '전국',
+    title: '재난재난제목',
+    content:
+      '재난에 대한 상세 내용 재난에 대한 상세 내용재난에 대한 상세 내용 재난에 대한 상세 내용재난에 대한 상세 내용 재난에 대한 상세 내용재난에 대한 상세 내용 재난에 대한 상세 내용재난에 대한 상세 내용',
+  },
+];
 
 export default function Setting() {
+  const [selectedTab, setSelectedTab] = useState('전체');
+
+  const handleTabPress = (tabName: string) => {
+    setSelectedTab(tabName);
+  };
+
   return (
-    <View>
-      <Text>알림화면</Text>
-    </View>
+    <ScrollView style={styles.layout}>
+      <View style={styles.tabContainer}>
+        <Pressable
+          style={
+            selectedTab === '전체' ? StyleSheet.compose(styles.tab, styles.selectedTab) : styles.tab
+          }
+          onPress={() => handleTabPress('전체')}
+        >
+          <Text
+            style={
+              selectedTab === '전체'
+                ? StyleSheet.compose(styles.tabText, styles.selectedTabText)
+                : styles.tabText
+            }
+          >
+            전체
+          </Text>
+        </Pressable>
+        <Pressable
+          style={
+            selectedTab === '전국' ? StyleSheet.compose(styles.tab, styles.selectedTab) : styles.tab
+          }
+          onPress={() => handleTabPress('전국')}
+        >
+          <Text
+            style={
+              selectedTab === '전국'
+                ? StyleSheet.compose(styles.tabText, styles.selectedTabText)
+                : styles.tabText
+            }
+          >
+            전국
+          </Text>
+        </Pressable>
+        <Pressable
+          style={
+            selectedTab === '우리동네'
+              ? StyleSheet.compose(styles.tab, styles.selectedTab)
+              : styles.tab
+          }
+          onPress={() => handleTabPress('우리동네')}
+        >
+          <Text
+            style={
+              selectedTab === '우리동네'
+                ? StyleSheet.compose(styles.tabText, styles.selectedTabText)
+                : styles.tabText
+            }
+          >
+            우리동네
+          </Text>
+        </Pressable>
+      </View>
+
+      <View style={styles.cautionContainer}>
+        <FeatherIcon name="info" size={12} color={COLOR.gray} />
+        <Text style={styles.cautionText}>알림 내역은 30일 후, 자동 삭제 돼요</Text>
+      </View>
+
+      <View style={styles.notiLayout}>
+        <View style={styles.notiWrapper}>
+          <View style={styles.notiWrapperTop}>
+            <Text style={styles.notiWDate}>2023-09-27</Text>
+            <Text style={styles.allRead}>다 읽음</Text>
+          </View>
+          <View style={styles.notiContainer}>
+            <View style={styles.notiTitleContainer}>
+              <View style={styles.notiTitleContainerLeft}>
+                <View style={styles.nationwideNotiCircle} />
+                <Text style={styles.notiCatecoryText}>전국 재난 알림</Text>
+              </View>
+              <Text style={styles.notiTime}>오전 09:30</Text>
+            </View>
+            <View style={styles.notiContent}>
+              <Text style={styles.notiContentText}>재난내용에 대한 것 재난 내용에 대한 것</Text>
+              <FaIcon name="angle-down" size={20} color={COLOR.middleGray} />
+            </View>
+          </View>
+          <View style={styles.notiContainer}>
+            <View style={styles.notiTitleContainer}>
+              <View style={styles.notiTitleContainerLeft}>
+                <View style={styles.hometownNotiCircle} />
+                <Text style={styles.notiCatecoryText}>우리동네 재난 알림</Text>
+              </View>
+              <Text style={styles.notiTime}>오전 09:30</Text>
+            </View>
+            <View style={styles.notiContent}>
+              <Text style={styles.notiContentText}>재난내용에 대한 것 재난 내용에 대한 것</Text>
+              <FaIcon name="angle-down" size={20} color={COLOR.middleGray} />
+            </View>
+          </View>
+          <View style={styles.notiContainer}>
+            <View style={styles.notiTitleContainer}>
+              <View style={styles.notiTitleContainerLeft}>
+                <View style={styles.nationwideNotiCircle} />
+                <Text style={styles.notiCatecoryText}>전국 재난 알림</Text>
+              </View>
+              <Text style={styles.notiTime}>오전 09:30</Text>
+            </View>
+            <View style={styles.notiContent}>
+              <Text style={styles.notiContentText}>재난내용에 대한 것 재난 내용에 대한 것</Text>
+              <FaIcon name="angle-down" size={20} color={COLOR.middleGray} />
+            </View>
+          </View>
+        </View>
+
+        <View style={styles.notiWrapper}>
+          <View style={styles.notiWrapperTop}>
+            <Text style={styles.notiWDate}>2023-09-27</Text>
+            <Text style={styles.allRead}>다 읽음</Text>
+          </View>
+          <View style={styles.notiContainer}>
+            <View style={styles.notiTitleContainer}>
+              <View style={styles.notiTitleContainerLeft}>
+                <View style={styles.nationwideNotiCircle} />
+                <Text style={styles.notiCatecoryText}>전국 재난 알림</Text>
+              </View>
+              <Text style={styles.notiTime}>오전 09:30</Text>
+            </View>
+            <View style={styles.notiContent}>
+              <Text style={styles.notiContentText}>재난내용에 대한 것 재난 내용에 대한 것</Text>
+              <FaIcon name="angle-down" size={20} color={COLOR.middleGray} />
+            </View>
+          </View>
+        </View>
+      </View>
+    </ScrollView>
   );
 }
+
+const styles = StyleSheet.create({
+  layout: {
+    paddingHorizontal: 15,
+    paddingTop: 20,
+  },
+  tabContainer: {
+    flexDirection: 'row',
+    gap: 10,
+    marginBottom: 10,
+  },
+  tab: {
+    paddingBottom: 10,
+    flexDirection: 'row',
+    gap: 7,
+  },
+  tabText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: `${COLOR.middleGray}`,
+  },
+  selectedTab: {
+    borderBottomWidth: 2,
+  },
+  selectedTabText: {
+    color: `${COLOR.black}`,
+  },
+  cautionContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 5,
+    backgroundColor: `${COLOR.lightBlue}`,
+    padding: 10,
+    borderRadius: 5,
+    marginBottom: 20,
+  },
+  cautionText: {
+    fontSize: 12,
+    color: `${COLOR.gray}`,
+  },
+  notiLayout: {},
+  notiWrapper: {
+    gap: 10,
+    marginBottom: 50,
+  },
+  notiWrapperTop: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  notiWDate: {
+    fontSize: 14,
+    fontWeight: '600',
+  },
+  allRead: {
+    fontSize: 10,
+    color: `${COLOR.blue}`,
+  },
+  notiContainer: {
+    backgroundColor: `${COLOR.white}`,
+    borderRadius: 5,
+    paddingVertical: 7,
+    ...Platform.select({
+      ios: {
+        shadowColor: `${COLOR.black}`,
+        shadowOpacity: 0.1,
+        shadowRadius: 5,
+        shadowOffset: {
+          width: 0,
+          height: 1,
+        },
+      },
+      android: {
+        elevation: 3,
+      },
+    }),
+  },
+  notiTitleContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    borderBottomWidth: 1,
+    borderColor: `${COLOR.lightGray}`,
+    flex: 1,
+    padding: 10,
+  },
+  notiTitleContainerLeft: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 10,
+  },
+  nationwideNotiCircle: {
+    borderRadius: 50,
+    backgroundColor: `${COLOR.notiNationWide}`,
+    width: 10,
+    height: 10,
+  },
+  hometownNotiCircle: {
+    borderRadius: 50,
+    backgroundColor: `${COLOR.notiHomeTown}`,
+    width: 10,
+    height: 10,
+  },
+  notiCatecoryText: {
+    fontSize: 14,
+    fontWeight: '500',
+  },
+  notiTime: {
+    fontSize: 10,
+    fontWeight: '500',
+    color: `${COLOR.gray}`,
+  },
+  notiContent: {
+    padding: 15,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  notiContentText: {
+    fontSize: 12,
+    color: `${COLOR.gray}`,
+  },
+});
